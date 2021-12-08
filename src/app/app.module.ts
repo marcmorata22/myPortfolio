2e60228822af9f5fa463cbd868e17a423425dfad
marcmorata22 <perezmoratamarc@gmail.com>
2021-12-08 13:48:38 +0000

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+   AppComponent,
+   PortfolioComponent,
+   HeaderComponent,
+   NavbarComponent
   ],
   imports: [
     BrowserModule,
