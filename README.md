dd575d5e90d89a703ef64f7d12b2b878af86cea7
marcmorata22 <perezmoratamarc@gmail.com>
2022-04-06 20:25:23 +0100

    money

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
-## Icons
+## Tutorial
     <script src="https://kit.fontawesome.com/a55cf2bb1e.js" crossorigin="anonymous"></script>
     npm i @fortawesome/fontawesome-free
     npm install bootstrap jquery @popperjs/core
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
